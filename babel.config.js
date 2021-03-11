1d16381bd22a6acd1fa98f08ea75453c7fe80e36
Vinicius de Moraes <vimoraescarvalho@gmail.com>
2021-03-11 18:45:49 -0300

    Convert project to use Typescript

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   presets: [
     "@babel/preset-env",
-
+    "@babel/preset-typescript",
     [
       "@babel/preset-react",
       {
-        runtime: "automatic" /* Não import React em todo arquivo com jsx */,
+        runtime: "automatic" /* Não importa React em todo arquivo com jsx */,
       },
     ],
   ],
